ac6bd28a0ceefc45209c5da799672bcc23091e01
Esteban Beerens <esteban.beerens@gmail.com>
2020-01-23 13:11:30 +0100

    Added first version of map which loads on CurrentLocation

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:loading/indicator/ball_grid_pulse_indicator.dart';
+import 'package:loading/loading.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  State<MapPage> createState() => MapPageState();
+}
+
+class MapPageState extends State<MapPage> {
+  GoogleMapController mapController;
+  Position _currentPosition;
+
+  void _onMapCreated(GoogleMapController controller) {
+      mapController = controller;
+  }
+
+  Future<void> _getLocation() async {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Center(
-        child: new Text("Map", style: new TextStyle(fontSize: 35.0),),
+    _getLocation();
+    if (_currentPosition == null) {
+      return Scaffold(
+        body: Container(
+        child: Center(
+          child: Loading(indicator: BallGridPulseIndicator(), size: 100.0,color: Color(0x39B1C3).withOpacity(0.9)),
         ),
-    );
+      ),
+      );
+    } else {
+      return Scaffold(
+        body: GoogleMap(
+          compassEnabled: true,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+            zoom: 15.0,
+          ),
+        ),
+      );
+    }
   }
 }
