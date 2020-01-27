2f19eca2ef84c3e691a0e1c9cf6407c211411624
Esteban Beerens <esteban.beerens@gmail.com>
2020-01-27 15:33:54 +0100

    Added tooltips to the custom markers and added minor open() functionalities to them for the Floating Panel

---
+++ lib/mock/marker_item_mock.dart
@@ -0,0 +1,7 @@
+import 'package:port_tracker/models/Marker_item.dart';
+
+  final markerItems = [
+    new MarkerItem(0, "Truck 1", "assets/images/map_markers/truck/cyan.png", 51.158315, 4.967445),
+    new MarkerItem(1, "Load 1", "assets/images/map_markers/load/red.png", 51.158401, 4.968479),
+    new MarkerItem(2, "Load 2", "assets/images/map_markers/load/cyan.png", 51.157959, 4.961615),
+  ];
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,21 +1,17 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:loading/indicator/ball_grid_pulse_indicator.dart';
 import 'package:loading/loading.dart';
+import 'package:port_tracker/components/floating_panel.dart';
+import 'package:port_tracker/mock/marker_item_mock.dart';
+import 'package:port_tracker/models/Marker_item.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-
-//Let's define a DrawerItem data object
-class MarkerItem {
-  String title;
-  String icon;
-  double lat;
-  double lng;
-  MarkerItem(this.title, this.icon, this.lat, this.lng);
-}
+// Class for the markers
 
 class MapPage extends StatefulWidget {
   @override
@@ -23,18 +19,14 @@ class MapPage extends StatefulWidget {
 }
 
 class MapPageState extends State<MapPage> {
-  GoogleMapController mapController;
   Position _currentPosition;
-
-  final markerItems = [
-    new MarkerItem("Truck 1", "assets/images/map_markers/truck/cyan.png", 51.158315, 4.967445),
-    new MarkerItem("Load 1", "assets/images/map_markers/load/red.png", 51.158401, 4.968479),
-    new MarkerItem("Load 2", "assets/images/map_markers/load/cyan.png", 51.157959, 4.961615),
-  ];
   
   List<Marker> allMarkers = [];
-  BitmapDescriptor pinLocationIcon;
+  MarkerItem selectedMarker;
 
+  PanelController floatingPanel = new PanelController();
+
+  // Call _getLocation() and _createMarkers() when loading the page
   @override
   void initState() {
     super.initState();
@@ -46,12 +38,9 @@ class MapPageState extends State<MapPage> {
   void dispose() { 
     super.dispose();
   }
-  
-  BorderRadiusGeometry radius = BorderRadius.only(
-    topLeft: Radius.circular(24.0),
-    topRight: Radius.circular(24.0),
-  );
 
+  // Function that gets the current location and puts it in _currentPosition
+  // You can pull the lat and lng from _currentposition by using .latitude and .longitude
   Future<void> _getLocation() async {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
     geolocator
@@ -65,6 +54,7 @@ class MapPageState extends State<MapPage> {
     });
   }
 
+  // Creates all the markers
   void _createMarkers() {
     for(var item in markerItems) {
       // BitmapDescriptor.fromAssetImage(ImageConfiguration(devicePixelRatio: 2.5), item.icon).then((onValue) {
@@ -74,50 +64,70 @@ class MapPageState extends State<MapPage> {
       // });
       allMarkers.add(
         Marker(
-          markerId: MarkerId(item.title),
+          markerId: MarkerId(item.id.toString()),
           icon: BitmapDescriptor.fromAsset(item.icon),
           // icon: pinLocationIcon,
           draggable: true,
-          position: LatLng(item.lat, item.lng)
+          position: LatLng(item.lat, item.lng),
+          onTap: () {
+            selectedMarker = markerItems[item.id];
+            log("Selected marker " + item.id.toString() + ": " + item.title.toString());
+          },
+          infoWindow: InfoWindow(
+            title: item.title,
+            snippet: "Click for info",
+            onTap: () {
+              floatingPanel.open();
+            }
+          )
         )
       );
     }
   }
 
+  // The floating panel when it's collapsed
   Widget _floatingCollapsed(){
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
-      ),
-      margin: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
-      child: Center(
-        child: Icon(Icons.drag_handle),
-      ),
+    return new GestureDetector(
+      onTap: () {
+        floatingPanel.open();
+      },
+      child: new Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
+        ),
+        margin: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+        child: Center(
+          child: Icon(Icons.drag_handle),
+        ),
+      )
     );
   }
 
+  // The floating panel when it's opened
   Widget _floatingPanel(){
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(24.0)),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 20.0,
-            color: Colors.grey,
-          ),
-        ]
-      ),
-      margin: const EdgeInsets.all(24.0),
-      child: Center(
-        child: Text("This is the SlidingUpPanel when open"),
-      ),
-    );
+      return Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(24.0)),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20.0,
+              color: Colors.grey,
+            ),
+          ]
+        ),
+        margin: const EdgeInsets.all(24.0),
+        child: Center(
+          child: Text("No marker has been selected.")
+        ),
+      );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_currentPosition == null) {    
+    // Check if the current position is known
+    if (_currentPosition == null) {
+      // The loading screen    
       return Scaffold(
         body: Container(
           child: Center(
@@ -126,6 +136,7 @@ class MapPageState extends State<MapPage> {
       ),
     );
     } else {
+      // The map screen
       return Scaffold(
         body: new SlidingUpPanel(
           renderPanelSheet: false,
@@ -133,22 +144,27 @@ class MapPageState extends State<MapPage> {
           collapsed: _floatingCollapsed(),
           body: Center(
             child: GoogleMap(
+              // Settings for the map
               compassEnabled: true,
               myLocationButtonEnabled: true,
               myLocationEnabled: true,
               buildingsEnabled: true,
+              // Set starting position on your current location
               initialCameraPosition: CameraPosition(
                 target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
                 zoom: 18.0,
               ),
               onMapCreated: (GoogleMapController controller) {},
+              // Set the markers on the map
               markers: Set.from(allMarkers)
             ),
           ),
+          // Settings for backdrop when the sliding panel is opened
           backdropEnabled: true,
           backdropOpacity: 0.2,
-          minHeight: 90,
-          borderRadius: radius,
+          // Hiehgt of sliding panel when closed
+          minHeight: 100,
+          controller: floatingPanel,
           ),
       );
     }
