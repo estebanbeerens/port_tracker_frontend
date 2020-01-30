f54d188c26991f17fb96de44312d359dca74c98b
Esteban Beerens <esteban.beerens@gmail.com>
2020-01-30 09:12:53 +0100

    Created new custom cards with a custom text theme

---
+++ lib/ui/styles/text_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static final baseTextStyle = const TextStyle(
+    fontFamily: 'Poppins'
+  );
+  static final smallTextStyle = commonTextStyle.copyWith(
+    fontSize: 9.0,
+  );
+  static final commonTextStyle = baseTextStyle.copyWith(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400
+  );
+  static final titleTextStyle = baseTextStyle.copyWith(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600
+  );
+  static final headerTextStyle = baseTextStyle.copyWith(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w400
+  );
+}
